9918e0b5afca87aa71674570d94a2dbe2898f6e6
Vineel Kumar Reddy Kovvuri <vineel.kovvuri@gmail.com>
2016-02-29 00:15:31 +0530

    Added ExploreJavaThreads package
    Signed-off-by: Vineel Kumar Reddy Kovvuri <vineel.kovvuri@gmail.com>

--- src/com/vineel/CoreJavaVolIHorstmann/Temp.java
+++ src/com/vineel/CoreJavaVolIHorstmann/Temp.java
@@ -4,4 +4,8 @@ package com.vineel.CoreJavaVolIHorstmann;
  * Created by vineel on 28/2/16.
  */
 public class Temp {
+    public static void main(String[] args) {
+        Throwable t = new Throwable();
+
+    }
 }
